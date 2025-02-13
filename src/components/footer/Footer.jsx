dba0b7e0793d91ed9ac613578ad50349b135592f
manjeet <manjeetkarnwal1027@gmail.com>
2025-02-13 18:14:24 +0530

    Home Page

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import styles from "./footer.module.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.logo}>devify</div>
+      <div className={styles.text}>
+        Devify creative thoughts agency © All rights reserved.
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Links from './links/Links'
 import styles from "./navbar.module.css"
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
-    <div className={styles.logo}>Navbar</div>
+    <Link href="/" className={styles.logo}>Navbar</Link>
     <Links />
     </div>
   )
